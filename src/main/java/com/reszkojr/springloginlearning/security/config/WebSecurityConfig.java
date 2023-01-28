c95262b0e102bb3bca83b65fe1f536eab9f91073
reszkojr <s.reszkojr@gmail.com>
2023-01-28 11:34:52 -0300

    Spring security endpoints

--- src/main/java/com/reszkojr/springloginlearning/security/config/WebSecurityConfig.java
+++ src/main/java/com/reszkojr/springloginlearning/security/config/WebSecurityConfig.java
@@ -1,4 +1,43 @@
 package com.reszkojr.springloginlearning.security.config;
 
-public class WebSecurityConfig {
+import com.reszkojr.springloginlearning.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@AllArgsConstructor
+@Configuration
+public class WebSecurityConfig  {
+
+    private final AppUserService service;
+    private final PasswordEncoder encoder;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .authorizeRequests()
+                        .antMatchers("/api/v*/registration/*")
+                        .permitAll()
+                    .anyRequest()
+                    .authenticated().and()
+                    .formLogin();
+            http.authenticationProvider(daoAuthenticationProvider());
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(encoder);
+        provider.setUserDetailsService(service);
+        return provider;
+    }
 }
