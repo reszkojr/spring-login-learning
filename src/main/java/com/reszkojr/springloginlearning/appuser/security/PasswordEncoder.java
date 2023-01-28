c987f255e4ddead48d26c7c6dfe7452d43aaa46e
reszkojr <s.reszkojr@gmail.com>
2023-01-28 11:09:49 -0300

    Move security package to springloginlearning package

--- src/main/java/com/reszkojr/springloginlearning/appuser/security/PasswordEncoder.java
+++
@@ -1,4 +0,0 @@
-package com.reszkojr.springloginlearning.appuser.security;
-
-public class PasswordEncoder {
-}
--- src/main/java/com/reszkojr/springloginlearning/appuser/security/config/WebSecurityConfig.java
+++
@@ -1,4 +0,0 @@
-package com.reszkojr.springloginlearning.appuser.security.config;
-
-public class WebSecurityConfig {
-}
