c1c0d973136c2b79e62751126baf9d01ac096f14
reszkojr <s.reszkojr@gmail.com>
2023-01-28 10:10:24 -0300

    Project created and application.yml configured

---
+++ src/main/java/com/reszkojr/springloginlearning/SpringLoginLearningApplication.java
@@ -0,0 +1,13 @@
+package com.reszkojr.springloginlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reszkojr/springloginlearning/SpringLoginLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reszkojr.springloginlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
